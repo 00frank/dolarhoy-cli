59e80772c81bb418266ef3380cd9f9b692de60d1
Frank <00frankgarcia@gmail.com>
2023-11-03 00:56:42 -0300

    adding chalk to project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const axios = require('axios');
 const cheerio = require('cheerio');
+const chalk = require('chalk');
 
 const System = {
   out: {
@@ -11,11 +12,11 @@ const System = {
 }
 
 const Dolar = {
-  mostrarCambio: function (info) {
-    if (info.text().includes("Venta"))
-      System.out.printLn(info.text().split('Venta').join(' Venta'));
-    else
-      System.out.printLn(info.text());
+  mostrarCambio: function ({ title, content}) {
+    System.out.printLn(title.text()); // dolar type title
+    let [buy, sell] = content.text().split("Venta") // dolar value ex. Compra$XXX.XXVenta$XXX.XX
+    buy = buy.split("Compra")[0];
+    System.out.printLn(`${!!buy && chalk.bgRed.underline(`Compra ${buy}`) + ' | '} ${chalk.bgGreen.underline(`Venta ${sell}`)}`)
   }
 }
 
@@ -34,13 +35,13 @@ async function main() {
     for (let i = 0; i < bloqueDolares.length - 1; i++) {
       let dolar = bloqueDolares[i]
       let info = $(dolar).children().toArray();
-      for (let j = 0; j < info.length; j++) {
-        Dolar.mostrarCambio($(info[j]))
-      }
+      Dolar.mostrarCambio({ title: $(info[0]), content: $(info[1]) });
+      // for (let j = 0; j < info.length; j++) {
+      // }
       System.out.printLn("=========");
     }
   } catch (error) {
-    System.out.printLn('Error al hacer la solicitud HTTP:'+ error);
+    System.out.printLn('Error al hacer la solicitud HTTP:' + error);
   }
 }
 
